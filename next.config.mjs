0e2cccccd03aecb5eda19370bbb8699755b5cdbd
Andrey <karpovich.andrey@gmail.com>
2024-09-07 20:57:42 +0300

    added gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",  // <=== enables static exports
-  basePath: "/house_bids_react",
-  reactStrictMode: true,
 };
 
 export default nextConfig;
