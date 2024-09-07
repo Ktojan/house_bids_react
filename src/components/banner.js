c6c59d2b40013c2dffb76180f6518dc9b816594f
Andrey <karpovich.andrey@gmail.com>
2024-09-07 17:03:16 +0300

    link to github added

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,6 +1,6 @@
 // import styles from "./banner.css"
 import { useContext } from "react";
-import { logo } from "./banner.module.css"
+import { logo, github } from "./banner.module.css"
 import { navigationContext } from "./app";
 import navValues from "../helpers/navValues";
 import cn from 'classnames';
@@ -13,13 +13,15 @@ const Banner = ({headerText, aboutProject}) => {
             <div className="col-3"> 
                 <img
                 onClick={() => navigate(navValues.home)}
-                // className={logo + ' pointer ' + bordered}
                 className={cn(logo, 'pointer')}  // example classnames
                 src="./logo2.jpeg" alt="logo" title="click to main page" />
             </div>
             <div className="col-8 text-xxl offset-1">
                 <h2>{headerText}</h2>
-                <p><i>{aboutProject}</i></p>
+                <p>
+                    <i>{aboutProject}</i>
+                    <a className={github} href="https://github.com/Ktojan/house_bids_react" target="_blank">code on Github</a>
+                </p>
             </div>
         </header>
     )
